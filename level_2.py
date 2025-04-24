77317e450a934d5ff681136795b7b4867c8c57ee
Leo Kavanaugh <leokavanaugh@Leos-MacBook-Pro-3.local>
2025-04-24 08:54:02 -0400

    level 2 complete

---
+++ level_2.py
@@ -0,0 +1,9 @@
+list = [1, "1", 1.0, True]
+
+
+playlist_for_coding = ["Heroes", "Two Horses", 
+                       "Tom Traubert's Blues (Four Sheets To The Wind)", 
+                       "Beside  You", "Eight Days A Week"]
+print(playlist_for_coding[0])
+
+playlist_for_coding.append("Timber Hearth")
